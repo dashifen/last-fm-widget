335b31e831324d0831b419dabb09db6b6c325f80
David Dashifen Kees <dashifen@dashifen.com>
2016-09-10 11:24:00 -0400

    added class to unknown album image

--- last-fm-widget.php
+++ last-fm-widget.php
@@ -6,7 +6,7 @@
  * Author URI:        http://dashifen.com
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * License:           GPL-2.0+
- * Version:           1.0.0
+ * Version:           1.0.1
  */
 
 if (!defined('WPINC')) {
